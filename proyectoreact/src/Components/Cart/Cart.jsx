26ed65480f5bed637b58b7ee5377d0843366d189
Carolina Giacchi <carogiacchi@gmail.com>
2023-11-18 13:17:29 -0300

    encarando el proyecto final

---
+++ proyectoreact/src/Components/Cart/Cart.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react"
+import { CartContext } from "../../context/CartContext";
+import CartItem from '../CartItem/CartItem'
+import { Link } from 'react-router-dom';
+import './Cart.css'
+const Cart = () => {
+      const {cart, clearCart, totalQuantity, total} = useContext(CartContext)
+      if(totalQuantity === 0){
+        return (
+        <div className="carrito-vacio">
+            <h1>No hay productos en el carrito</h1>
+            <Link to='/'  className="but"> Birritas </Link>
+          </div>
+        )
+      }
+      return (
+        <div>
+          { cart.map((item => <CartItem key={item.id} {...item}/> ))}
+          <div className="container">
+              <button onClick={() => clearCart() } className='Button' > Limpiar Carrito </button>
+              <Link to='/' className="Button">Volver a la Tienda</Link>
+              <Link to='/Checkout' className="Button">Finalizar Compra</Link>
+              <h3 className="total">Total: ${total}</h3>
+          </div>
+              
+        </div>
+      )
+}
+export default Cart
--- proyectoreact/src/Components/CartWidget/CartWidget.jsx
+++ proyectoreact/src/Components/CartWidget/CartWidget.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './CartWidget.css';
 import {TfiShoppingCart} from "react-icons/tfi";
+import {CartContext} from "../../Context/CartContext";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+
 
 const CartWidget = () => {
+  const { totalQuantity} = useContext(CartContext);
   return (
-    <>
-        <div className='div-cart'>
+      <div className='widgetCarrito'>
         <button className='button-cart'>
           <TfiShoppingCart style ={{color: "brown", fontSize: "1.5em"}} />        
         </button>
-        </div>
-    </>
+          <Link to="/Cart" style={{display: totalQuantity > 0 ? 'block' :'none'}}>
+          <span className="material-symbols-outlined">Shopping_cart</span>
+          {
+              totalQuantity > 0 && <strong className='cantidad'>{ totalQuantity}</strong>
+          }
+          </Link>
+      </div>
   )
 }
-
 export default CartWidget
